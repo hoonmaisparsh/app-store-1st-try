ce48c5df55790b97477bc59f3f40a897e33fd57a
hoonmaisparsh <146664669+hoonmaisparsh@users.noreply.github.com>
2025-02-20 20:35:28 +0530

    Update db_connection.py

--- mobile/store/db_connection.py
+++ mobile/store/db_connection.py
@@ -1,8 +1,8 @@
 import pymongo 
 import pymongo.mongo_client
-url ="mongodb+srv://singhsparsh2004: FfrFEVwZ90DFVyKv@sparsh.ug3fb.mongodb.net"
+url ="mongodb+srv://singhsparsh2004:FfrFEVwZ90DFVyKv@sparsh.ug3fb.mongodb.net/?retryWrites=true&w=majority&appName=sPARSH"
 
 client=pymongo.MongoClient(url)
 
-db=client["products"]
+db=client["store"]
 
